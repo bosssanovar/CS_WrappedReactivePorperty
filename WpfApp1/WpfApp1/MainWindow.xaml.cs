09e071d7268b9400e57eff35ba74aeaebf82723a
kei ito <ito.kei@jvckenwood.com>
2024-01-26 14:55:16 +0900

    first commit

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Reactive.Bindings;
+using System.Reactive.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+
+            Text = new ReactivePropertySlim<string>(InnerText);
+            Text.Subscribe(x => InnerText = x);
+
+            Display = Text.Select(x => x + "Disp.").ToReadOnlyReactivePropertySlim<string>();
+
+            InitCommand.Subscribe(_ => Init());
+
+
+            InitializeComponent();
+        }
+    }
+}
---
+++ WpfApp1/WpfApp1/ViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Reactive.Bindings;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Security.RightsManagement;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public partial class MainWindow
+    {
+        public ReactivePropertySlim<string> Text { get; }
+        public ReadOnlyReactivePropertySlim<string> Display { get; }
+
+        public string InnerText { get; set; } = "Init";
+
+        public ReactiveCommand InitCommand { get; } = new ReactiveCommand();
+
+        public void Init()
+        {
+            InnerText = "Init";
+        }
+    }
+}
